835e1c51020f143c63285898218d41457e1c07c8
Hung Dang <hungptit@gmail.com>
2015-11-26 10:41:13 -0500

    Add markdown setup

--- emacs/extra_features.el
+++ emacs/extra_features.el
@@ -120,3 +120,11 @@
 (require 'json-mode)
 (require 'json-reformat)
 (require 'json-snatcher)
+
+;; Markdown
+(add-to-list 'load-path (concat emacs-setup-root-path "markdown-mode"))
+(autoload 'markdown-mode "markdown-mode"
+  "Major mode for editing Markdown files" t)
+(add-to-list 'auto-mode-alist '("\\.text\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
